4e891bce4564034b2f5f2068155344307653573a
RushabRisal <rushabofficials@gmail.com>
2025-01-18 12:51:32 +0545

    optimized the model added forecast visualization

--- scripts/training/time_series_model.py
+++ scripts/training/time_series_model.py
@@ -1,17 +1,27 @@
 import pandas as pd
 from prophet import Prophet
-from joblib import dump
+import joblib
+from multiprocessing import Pool, cpu_count
 
-# Load and preprocess your data
 data = pd.read_csv('./data/cleanData.csv')
 data['Date'] = pd.to_datetime(data['Date'], errors='coerce')
 data = data.dropna(subset=['Date'])
-data = data.groupby(['Commodity', 'Date']).agg({'Average': 'mean'}).reset_index()
-data = data[['Commodity', 'Date', 'Average']].rename(columns={'Date': 'ds', 'Average': 'y'})
-commodity_groups = data.groupby('Commodity')
-def train_and_save_model(group, commodity_name):
+data = (
+    data.groupby(['Commodity', 'Date'])
+    .agg({'Average': 'mean'})
+    .reset_index()
+    .rename(columns={'Date': 'ds', 'Average': 'y'})
+)
+def train_model(commodity_group):
+    commodity_name, group_data = commodity_group
     model = Prophet()
-    model.fit(group)
-    dump(model, f'./models/time_prophet_model.pkl')
-for commodity, group in commodity_groups:
-    train_and_save_model(group, commodity)
+    model.fit(group_data)
+    model_filename = f"./models/time_prophet_model.joblib"
+    joblib.dump(model, model_filename)
+
+
+if __name__ == "__main__":
+    grouped_data = data.groupby('Commodity')
+    tasks = [(commodity, group) for commodity, group in grouped_data]
+    with Pool(cpu_count() - 1) as pool:  
+        results = pool.map(train_model, tasks)
